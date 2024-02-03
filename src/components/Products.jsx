fd5f40bcbd934ac44e1e61143c87b7fe41afc6bb
ADMIN <admin@MacBook-Air-de-ADMIN.local>
2024-02-03 01:37:54 -0500

    updated commerce

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 
+//     get all products
 const Products = () => {
+    useEffect(() => {
+    const getAllProducts = async() => {
+        const response = await fetch(`https://fakestoreapi.com/products`);
+        const responseJson = await response.json();
+        const Allproducts = responseJson.results;
+        console.log(responseJson);
+    }
+    getAllProducts();
+   })
+
     return (
         <h1>Our Products</h1>
     )
 }
-//     get all products
 
 //     get a single product
 //     get all catergories
